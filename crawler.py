45f5abfa018d88a845a0c5859ad607245db02528
arman-ashrafian <ashy63@gmail.com>
2018-11-20 22:24:55 -0800

    finish link scraping

--- crawler.py
+++ crawler.py
@@ -15,7 +15,45 @@ class Crawler:
                     self.links.append(x['link'])
 
         # get links in majors table
+        for x in dic['majors']:
+            # check image links
+            self.links.append(x.get('image'))
+            if x.get('moreInfo'):
+                for d in x['moreInfo']:
+                    # check links in moreInfo
+                    self.links.append(d.get('link'))
+        
+        # get links in departments table
+        for x in dic['departments'].values():
+            self.links.append(x['link'])
 
+        # get links in resources table
+        for x in dic['resources']:
+            if x.get('mapImage'):
+                self.links.append(x.get('mapImage'))
+            if x.get('mapLink'):
+                self.links.append(x.get('mapLink'))
+            if x.get('link'):
+                self.links.append(x.get('link'))
+        
+        # get links in resourceBanner table
+        for x in dic['resourceBanner']:
+            img = x.get('image')
+            link = x.get('link')
+            if img: self.links.append(img)
+            if link: self.links.append(link)
+        
+        # get links in projects table
+        for x in dic['projects']:
+            for v in x.get('videos'):
+                self.links.append(v)
+            if x.get('link'):
+                self.links.append(x.get('link'))
+        
+        # get links in orgs table
+        for x in dic['orgs']:
+            link = x.get('link')
+            if link: self.links.append(link)
 
     def readDatabase(self):
         with open(self.databaseName, encoding='utf-8') as f:
