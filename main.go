0c96b0089749988735f40478006acb6162166e48
Arman Ashrafian <ashy63@gmail.com>
2018-11-21 18:13:05 -0800

    format output

--- main.go
+++ main.go
@@ -7,6 +7,11 @@ import (
 	"os"
 )
 
+type responseObj struct {
+	link   string
+	status int
+}
+
 func main() {
 	const baseURL = "http://computingpaths.ucsd.edu"
 
@@ -25,7 +30,7 @@ func main() {
 		links = append(links, scanner.Text())
 	}
 
-	ch := make(chan string)
+	ch := make(chan *responseObj)
 	for _, l := range links {
 		if l[0] == '/' {
 			l = baseURL + l
@@ -35,22 +40,25 @@ func main() {
 		go makeRequest(l, ch)
 	}
 
-	for x := 0; x < 1000; x++ {
-		v, ok := <-ch
-		if !ok {
-			break
+	for x := 0; x < len(links); x++ {
+		v := <-ch
+		if v.status != 200 {
+			fmt.Printf("|%s| %-6s\n", "ERROR", v.link)
 		}
-		fmt.Printf("%d --", x)
-		fmt.Println(v)
 	}
 	fmt.Println("done")
 }
 
-func makeRequest(link string, ch chan<- string) {
+func makeRequest(link string, ch chan<- *responseObj) {
 	resp, err := http.Get(link)
+	respObj := new(responseObj)
+	respObj.link = link
+
 	if err != nil || resp.StatusCode != 200 {
-		ch <- fmt.Sprintf("%s  --  ERROR", link)
+		respObj.status = -1
+		ch <- respObj
 		return
 	}
-	ch <- fmt.Sprintf("%s  --  %s", link, resp.Status)
+	respObj.status = 200
+	ch <- respObj
 }
